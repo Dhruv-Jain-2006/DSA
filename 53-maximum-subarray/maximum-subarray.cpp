2787a8f93b34ca2d102580992fd3b4a2e1ba605c
Dhruv Jain <70910093+Dhruv-Jain-2006@users.noreply.github.com>
2026-07-22 21:25:44 +0530

    Time: 0 ms (100.00%) | Memory: 71.6 MB (98.75%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
+        // KADANE'S ALGORITTHM
+        // Make max and current sum... Then add each element one by one
+        // for each ele check if curr sum is negative, if yes then curr sum = 0
+        // So we start our subarray from the next element
+        
         int maxSum = INT_MIN, currSum = 0;
 
         for (int i=0; i < nums.size(); i++) {
             currSum += nums[i];
             maxSum = max(currSum, maxSum);
-            
+
             if (currSum < 0) currSum = 0;
         }
         return maxSum;
