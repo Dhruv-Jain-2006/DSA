db877b28c47da729a7265489ea6e7c7aa9b080a0
Dhruv Jain <70910093+Dhruv-Jain-2006@users.noreply.github.com>
2026-07-25 00:57:16 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (6.55%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void merge(vector<int>& A, int m, vector<int>& B, int n) {
+        int k = m+n-1;
+        int i = m-1, j = n-1;
+
+        while (i >= 0 && j >= 0) {
+            if (A[i] >= B[j]) {
+                A[k] = A[i];
+                i--;
+            } else {
+                A[k] = B[j];
+                j--;
+            }
+            k--;
+        }
+
+        while( j >= 0) {
+            A[k--] = B[j--];
+        }
+    }
+};
