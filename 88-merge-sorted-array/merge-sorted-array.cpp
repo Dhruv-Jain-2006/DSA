1194fa6998a7210889163cc3127f9280dc77f561
Dhruv Jain <70910093+Dhruv-Jain-2006@users.noreply.github.com>
2026-07-25 00:59:38 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (18.15%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     void merge(vector<int>& A, int m, vector<int>& B, int n) {
+        // Fill backwards comparing element wise A and B arrays
+
         int k = m+n-1;
         int i = m-1, j = n-1;
 
         while (i >= 0 && j >= 0) {
             if (A[i] >= B[j]) {
-                A[k] = A[i];
-                i--;
+                A[k--] = A[i--];
             } else {
-                A[k] = B[j];
-                j--;
+                A[k--] = B[j--];
             }
-            k--;
         }
 
         while( j >= 0) {
